c93a041a9c634ea3b41572d5ab340ce9e7e8e739
TolikBurda <tolik.burda@gmail.com>
2019-04-17 15:15:53 +0300

    Update newGame.js

--- newGame.js
+++ newGame.js
@@ -1,54 +1,16 @@
 let start = document.getElementById('start');
 let stop = document.getElementById('end');
 let twoPlayers = document.getElementById('two')
-start.addEventListener('click', startGame);
-stop.addEventListener('click', stopGame);
+//start.addEventListener('click', game.startGame);
+// stop.addEventListener('click', stopGame);
 // twoPlayers.addEventListener('click', function(e){
 
 // });
 const fieldWidth = 20;
 const fieldHeight = 20;
 const cellSize = 10;
-
-
-class Game {
-    constructor(arr, w, h){
-        this.fieldWidth = w;
-        this.fieldHeight = h;
-        this.intervalId = null;
-        this.arrOfSnakes = arr;
-        this.apple = { x: 0, y: 0 }
-        // this.snake = s;
-        // this.snake1 = s1
-    }
-    spawnApple(){ 
-        this.apple.x = Math.round(Math.random() * this.fieldWidth);
-        this.apple.y = Math.round(Math.random() * this.fieldHeight); 
-    }
-    // createSnakes(){
-    //     this.arrOfSnakes.push(snake)
-    //     this.arrOfSnakes.push(snake1)
-    //     console.log(this.arrOfSnakes);
-    // }
-    startGame(){
-        createSnakes();
-        this.intervalId = setInterval(mainLoop, 600);
-        spawnApple();
-    }
-    mainLoop(){
-
-    }
-    stopGame(){
-
-    }
-
-}
-
-const snake = new Snake(fieldWidth, fieldHeight, number1);
-const snake1 = new Snake(fieldWidth, fieldHeight, number2);
-
-const game = new Game([snake, snake1], fieldWidth, fieldHeight)
-
+const number1 = 1;
+const number2 = 2;
 
 class Snake {
     constructor(w, h, n){
@@ -155,8 +117,79 @@ class Snake {
         
     }
 }
-const snake = new Snake(fieldWidth, fieldHeight, number1);
-const snake1 = new Snake(fieldWidth, fieldHeight, number2);
+
+class Game {
+    constructor(arr, w, h, d){
+        this.fieldWidth = w;
+        this.fieldHeight = h;
+        this.intervalId = null;
+        this.arrOfSnakes = arr;
+        this.apple = { x: 0, y: 0 };
+        this.draw = d;
+        // this.snake = s;
+        // this.snake1 = s1;
+        //this.spawnApple();
+    }
+    
+    spawnApple(){ 
+        this.apple.x = Math.round(Math.random() * this.fieldWidth);
+        this.apple.y = Math.round(Math.random() * this.fieldHeight); 
+        console.log(this.apple);
+        
+    }
+    // createSnakes(){
+    //     this.arrOfSnakes.push(snake)
+    //     this.arrOfSnakes.push(snake1)
+    //     console.log(this.arrOfSnakes);
+    // }
+
+    snakesControl(){
+
+    }
+
+    stopGame(){
+        clearInterval(this.intervalId);
+    }
+
+    startGame(){
+        this.stopGame();
+        this.arrOfSnakes;
+        this.intervalId = setInterval(()=>{
+            this.mainLoop()
+        }, 600);
+        this.spawnApple();
+    }
+    mainLoop(){
+        for(let i = 0; i < this.arrOfSnakes.length; i++){
+            if(this.arrOfSnakes[i].checkAppleCollision(this.apple)){
+                this.arrOfSnakes[i].growSnake();
+                this.spawnApple()
+            }else{
+                this.arrOfSnakes[i].moveSnake();
+            }
+            if(this.arrOfSnakes[i].checkTailCollision()){
+                this.stopGame();
+            }else if(this.arrOfSnakes[i].checkBordersCollision()){
+                this.stopGame();
+            }
+            this.draw.drawField(this);
+        }
+    }
+    
+
+}
+
+document.addEventListener('keydown', function(e){
+    for(let i = 0; i < game.arrOfSnakes.length; i++){
+        game.arrOfSnakes[i].changeDirectionOfSnake(e)
+    }
+})
+
+
+
+
+
+
 
 class Drawing {
     constructor(w, h, c, g){
@@ -165,40 +198,41 @@ class Drawing {
         this.fieldWidth = w;
         this.fieldHeight = h;
         this.cellSize = c;
-        this.game = g;
+        //this.game = g;
         this.canvas.width = (this.fieldWidth + 1)*this.cellSize;
         this.canvas.height = (this.fieldHeight + 1)*this.cellSize; 
     }
 
+    drawField(game){   
+        this.ctx.fillStyle = 'white';
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
     
-
-    drawField(){   
-        ctx.fillStyle = 'white';
-        ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-    
-        ctx.strokeStyle = 'black';
-        for(let j = 0; j < this.game.arrOfSnakes.length; j++){
-            let snake = this.game.arrOfSnakes[j]
+        this.ctx.strokeStyle = 'black';
+        for(let j = 0; j < game.arrOfSnakes.length; j++){
+            let snake = game.arrOfSnakes[j]
             for(let i = 0; i < snake.segments.length; i++){
-                ctx.strokeRect(snake.segments[i].x*this.cellSize, snake.segments[i].y*this.cellSize, this.cellSize, this.cellSize);
+                this.ctx.strokeRect(snake.segments[i].x*this.cellSize, snake.segments[i].y*this.cellSize, this.cellSize, this.cellSize);
             }
+            //console.log(snake.segments[i].x)
         }
         
-        ctx.strokeStyle = 'magenta';
-        ctx.strokeRect(this.game.apple.x*this.cellSize, this.game.apple.y*this.cellSize, this.cellSize, this.cellSize);
+        this.ctx.strokeStyle = 'magenta';
+        this.ctx.strokeRect(game.apple.x*this.cellSize, game.apple.y*this.cellSize, this.cellSize, this.cellSize);
     
-        ctx.strokeStyle = 'blue';
-        ctx.strokeRect(0, 0, (this.fieldWidth + 1)*this.cellSize, (this.fieldHeight + 1)*this.cellSize);
+        this.ctx.strokeStyle = 'blue';
+        this.ctx.strokeRect(0, 0, (this.fieldWidth + 1)*this.cellSize, (this.fieldHeight + 1)*this.cellSize);
     }
 }
-const draw = new Drawing(fieldWidth, fieldHeight, cellSize, game)
+
+const snake = new Snake(fieldWidth, fieldHeight, number1);
+const snake1 = new Snake(fieldWidth, fieldHeight, number2);
+const draw = new Drawing(fieldWidth, fieldHeight, cellSize)
+const game = new Game([snake, snake1], fieldWidth, fieldHeight, draw)
+
+start.addEventListener('click', function(){                        ///()=>
+    game.startGame()
+} );
 
 
 
 
-
-document.addEventListener('keydown', function(e){
-    for(let i = 0; i < arrOfSnakes.length; i++){
-        arrOfSnakes[i].changeDirectionOfSnake(e)
-    }
-})
